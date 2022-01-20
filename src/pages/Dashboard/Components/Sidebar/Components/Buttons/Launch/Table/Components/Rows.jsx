d9abe4e82b95e67f926fccda611a0a6a2f5be376
Alan-Graton <graton.alan@gmail.com>
2022-01-19 23:38:18 -0300

    "Project" button prototype created

--- src/pages/Dashboard/Components/Sidebar/Components/Buttons/Launch/Table/Components/Rows.jsx
+++ src/pages/Dashboard/Components/Sidebar/Components/Buttons/Launch/Table/Components/Rows.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import DaySummary from "../../Time Logic/DaySummary";
+import SelectProjectBtn from "./SelectProjectBtn";
 
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
@@ -22,28 +23,30 @@ function Rows({ projectCardShow }) {
   return (
     <div>
       <TableRow className={classes.tableRow}>
-      <TableCell>
-        <input type="date" className={classes.dateInput} />
-      </TableCell>
-      <TableCell>
-        <Button className={classes.select_create_Project}>Projects</Button>
-      </TableCell>
-      <TableCell>
-        <Typography className={classes.componenteLabels} variant="button">
-          Description
-        </Typography>
-        <TextField color="secondary" sixe="small" />
-      </TableCell>
-      <TableCell>
-        <DaySummary
-          changeNotifyStart={(v) => dailySummary(0, v)}
-          changeNotifyEnd={(v) => dailySummary(0, v)}
-        />
-      </TableCell>
-      <TableCell>
-        <Button onClick={projectCardShow} className={classes.addProject}>Add</Button>
-      </TableCell>
-    </TableRow>
+        <TableCell>
+          <input type="date" className={classes.dateInput} />
+        </TableCell>
+        <TableCell>
+          <SelectProjectBtn />
+        </TableCell>
+        <TableCell>
+          <Typography className={classes.componenteLabels} variant="button">
+            Description
+          </Typography>
+          <TextField color="secondary" sixe="small" />
+        </TableCell>
+        <TableCell>
+          <DaySummary
+            changeNotifyStart={(v) => dailySummary(0, v)}
+            changeNotifyEnd={(v) => dailySummary(0, v)}
+          />
+        </TableCell>
+        <TableCell>
+          <Button onClick={projectCardShow} className={classes.addProject}>
+            Add
+          </Button>
+        </TableCell>
+      </TableRow>
     </div>
   );
 }
--- src/pages/Dashboard/Components/Sidebar/Components/Buttons/Launch/Table/Components/styles/Rows.js
+++ src/pages/Dashboard/Components/Sidebar/Components/Buttons/Launch/Table/Components/styles/Rows.js
@@ -27,20 +27,31 @@ const useStyles = makeStyles((theme) => ({
             boxShadow: "0 3px 5px 2px rgba(28, 133, 12, .3)",
         },
     },
-    componenteLabels: {
-        color: "#989c9c",
-    },
-    select_create_Project: {
+    selectCreateProject: {
         borderRadius: 5,
         color: "white",
         height: 40,
         marginTop: 20,
-        background: "linear-gradient(45deg, #f29f05 30%, #ed6f00 90%)",
+        background: "linear-gradient(45deg, #11aebf 30%, #0ddbae 90%)",
         "&:hover": {
-        background: "linear-gradient(45deg, #61ba0b 30%, #7bf20c 90%)",
-        boxShadow: "0 3px 5px 2px rgba(28, 133, 12, .3)",
+            background: "linear-gradient(45deg, #0a7694 30%, #0a3a94 90%)",
+            boxShadow: "0 3px 5px 2px rgba(28, 133, 12, .3)",
         },
-    }
+    },
+    createNewProject: {
+        borderRadius: 5,
+        color: "white",
+        height: 40,
+        marginTop: 20,
+        background: "linear-gradient(45deg, #0cc72b 30%, #0cc779 90%)",
+        "&:hover": {
+            background: "linear-gradient(45deg, #c1c70c 30%, #d0d4a5 90%)",
+            boxShadow: "0 3px 5px 2px rgba(28, 133, 12, .3)",
+        },
+    },
+    componenteLabels: {
+        color: "#989c9c",
+    },
 }))
 
 export default useStyles;
